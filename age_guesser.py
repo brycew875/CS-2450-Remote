3b86913385c83bef70b9d2bc214ddaf4fff3bc8a
Bryce Williams <brycew875@gmail.com>
2026-01-20 10:08:48 -0700

    added attempt counter

--- age_guesser.py
+++ age_guesser.py
@@ -1,14 +1,18 @@
-import random 
+import random
 
-print("HI! I am going to try and guess your age")
-name = input("What is your name?")
+print("Hi! I am going to try to guess your age.")
+name = input("What is your name? ")
 
-while True: 
-    guess = random.randint(15,30)
-    answer = input(f"Is your age{guess}? (y/n):")
+tries = 0
+
+while True:
+    guess = random.randint(15, 30)
+    tries += 1
+    answer = input(f"Is your age {guess}? (y/n): ")
 
     if answer == 'y':
-        print(f"{name} is {guess} years old")
-        break 
-    else: 
+        print(f"{name} is {guess} years old.")
+        print(f"It took me {tries} tries!")
+        break
+    else:
         print("Rats.")
